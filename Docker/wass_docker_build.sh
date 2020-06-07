31aace15756569ee5bb101e795094c81b07e4ad1
Filippo Bergamasco <fbergama@gmail.com>
2020-06-07 14:11:55 +0200

    wass_docker_build.sh script fix

--- Docker/wass_docker_build.sh
+++ Docker/wass_docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#docker build --no-cache=true --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg VCS_REF=$(sed -n -e 's/^SET( WASS_VERSION_STR \"\(...\).* .*/v\1_/p' src/CMakeLists.txt)$(git describe --all --long) -t bergamasco/wass:latest .
+docker build --no-cache=true --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg VCS_REF=$(sed -n -e 's/^SET( WASS_VERSION_STR \"\(...\).* .*/v\1_/p' src/CMakeLists.txt)$(git describe --all --long) -t bergamasco/wass:latest .
 
 docker build --no-cache=true -f Docker/Dockerfile.runall -t bergamasco/wass:runall .
 
